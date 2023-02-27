e03bb4c882827b6411ea779ea12b537e71444e78
juliamouat <60278122+juliamouat@users.noreply.github.com>
2023-02-27 11:00:58 -0800

    Update DM.R

--- DM.R
+++ DM.R
@@ -1,5 +1,6 @@
-#identify DMRs for individuals with Down Syndrome based on whether they have a major heart defect or not 
-#Ben Laufer pipeline
+#identify DMRs for individuals with Down Syndrome and a Major Heart Defect (DS MHD) compared with individuals with Down Syndrome and no Major Heart Defect (DS non-MHD)
+#Sex Combined and Sex Stratified (Males Only and Females Only) analyses
+#developed by Ben Laufer: https://github.com/ben-laufer/DMRichR
 
 cd /share/lasallelab/Jules/DS_NDBS/cytosine_reports
 
@@ -18,7 +19,7 @@ Rscript \
 --maxBlockPerms 10 \
 --cutoff '0.05' \
 --testCovariate Major_heart_defect \
---adjustCovariate 'Sex' \
+--adjustCovariate 'Sex;AgeBloodCollection;CD8T;CD4T;NK;Bcell;Mono;Gran;nRBC' \
 --sexCheck TRUE \
 --GOfuncR TRUE \
 --cores 20 \
